777a0ab2f080c0e5733cb0eb21b51874a84115e9
Ian Price <michael.ian.price@gmail.com>
2017-07-04 20:18:06 -0400

    Basic task class created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Task {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Task Manager';
+  task: Task = {
+    id: 1,
+    name: 'Add tasks to manage'
+  };
 }
