afb84904d01a4d65de0a7449b4287be21c43920c
Ian Price <michael.ian.price@gmail.com>
2017-07-16 21:44:53 -0400

    creating service ng4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from './task';
+import { TaskService } from './task.service';
+
 
 const TASKS: Task[] = [
   { id: 11, name: 'ng4 app' },
@@ -10,13 +12,21 @@ const TASKS: Task[] = [
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TaskService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private taskService: TaskService) { }
   title = 'Task Manager';
-  tasks = TASKS;
+  tasks: Task[];
   selectedTask: Task;
   onSelect(task: Task): void {
     this.selectedTask = task;
   };
+  getTasks(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+  ngOnInit(): void {
+    this.getTasks();
+  }
 }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,7 @@
+import { Task } from './task';
+
+export const TASKS: Task[] = [
+  { id: 11, name: 'ng4 app' },
+  { id: 12, name: 'acquisition pipeline app' },
+  { id: 13, name: '3D 2048' }
+];
