f2a06eb9e6c4b8878fd1228bd7864154248ad54a
Ian Price <michael.ian.price@gmail.com>
2017-07-19 22:15:28 -0400

    building out dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Task } from './task';
+import { TaskService } from './task.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks()
+      .then(tasks => this.tasks = tasks.slice(1, 5));
+  }
+
+
+}
